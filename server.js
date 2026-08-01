8633433d10ef29742b51a49e0d3646529c98142e
Frontendcanvas <sangita.workdev@gmail.com>
2026-08-02 00:08:54 +0530

    node server added with express now its render on 4000 a wohe app

---
+++ server.js
@@ -0,0 +1,18 @@
+ require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = process.env.PORT || 4000
+app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('Hello Twitter!');
+})
+app.get('/order', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
